1d024ebec0b5a3b8ebc9ada884e3d0cc3c5e91e6
Sakki200 <neil.baldeck@gmail.com>
2024-07-19 12:16:31 +0200

    patch logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import SearchBarHome from "../components/SearchBar_Home";
+import ReacletteLogoV2 from "../../public/img/reaclette_home_logo.png";
 
 export default function Home() {
   return (
@@ -7,10 +8,7 @@ export default function Home() {
       <Header />
       <main className="home">
         <figure>
-          <img
-            src="../../public/img/reaclette_home_logo.png"
-            alt="Logo Reaclette v2"
-          />
+          <img src={ReacletteLogoV2} alt="Logo Reaclette v2" />
         </figure>
         <h1>TROUVE LA RECETTE DE TON PLAT FAVORIS</h1>
         <SearchBarHome />
