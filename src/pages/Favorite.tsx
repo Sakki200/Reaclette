9b5e99ea97401ca597b008665fb61699a7e5ae2e
Sakki200 <neil.baldeck@gmail.com>
2024-07-19 14:49:56 +0200

    showFavorite css done

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -2,6 +2,9 @@ import DeleteLocalStorage from "../components/DeleteLocalStorage";
 import Header from "../components/Header";
 import PostLocalStorage from "../components/PostLocalStorage";
 import { NavLink } from "react-router-dom";
+import FavOn from "../../public/img/fav_on.png";
+import FavOff from "../../public/img/fav_off.png";
+import { useState } from "react";
 export default function Favorite() {
   const getItem = localStorage.getItem("reaclette");
   const allFavorites: { id: number; name: string }[] = getItem
@@ -12,11 +15,16 @@ export default function Favorite() {
     const favButton = document.getElementById(
       "favCheckBox" + id
     ) as HTMLInputElement;
+    const favIcon = document.getElementById("favIcon" + id) as HTMLInputElement;
+
+    favButton.checked = !favButton.checked;
 
     if (favButton.checked) {
       PostLocalStorage(id, name);
+      favIcon.src = FavOn;
     } else {
       DeleteLocalStorage(id);
+      favIcon.src = FavOff;
     }
   };
 
@@ -25,18 +33,32 @@ export default function Favorite() {
       <Header />
       <main className="showFavorites">
         <h1>VOS FAVORIS</h1>
-        {allFavorites.map((favorite: { id: number; name: string }) => (
-          <div key={favorite.id}>
-            <NavLink to={"/receipe/" + favorite.name}>{favorite.name}</NavLink>
-            <input
-              type="checkbox"
-              id={"favCheckBox" + favorite.id}
-              className="addToFavoris"
-              onClick={() => LocalStorage(favorite.id, favorite.name)}
-              defaultChecked={true}
-            ></input>
-          </div>
-        ))}
+        <div className="allFavorites">
+          {allFavorites.map((favorite: { id: number; name: string }) => (
+            <div key={favorite.id}>
+              <NavLink to={"/receipe/" + favorite.id}>
+                {favorite.name.toUpperCase().replace(/_/g, " ")}
+                <img
+                  id={"favIcon" + favorite.id}
+                  className="favIcon"
+                  src={FavOn}
+                  alt="Logo favoris"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    LocalStorage(favorite.id, favorite.name);
+                  }}
+                />
+                <input
+                  type="checkbox"
+                  id={"favCheckBox" + favorite.id}
+                  className="addToFavoris"
+                  defaultChecked={true}
+                />
+              </NavLink>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
